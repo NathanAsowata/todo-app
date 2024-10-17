f1d4fe3f45833aec7a2dd82c3046843e705927c7
Nathan <nathanasowata@gmail.com>
2024-10-17 08:43:38 +0100

    added help command

--- src/main/java/com/nathanasowata/Main.java
+++ src/main/java/com/nathanasowata/Main.java
@@ -1,17 +1,103 @@
 package com.nathanasowata;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Scanner scanner = new Scanner(System.in);
+        Task tasks = new Task();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+
+        System.out.println("Add a task: ");
+        String firstTask = scanner.nextLine();
+        tasks.addNewTask(firstTask);
+
+        System.out.println("""
+                Instructions:\s
+                 add: adds a new task\
+                \s
+                 show: shows all tasks\
+                \s
+                 delete: deletes a task\
+                \s
+                 exit: to end the program""");
+
+        System.out.println("Your call: ");
+        String userInput = scanner.nextLine().toLowerCase();
+        handlePrompt(userInput, scanner, tasks);
+    }
+
+    static void handlePrompt (String userInput, Scanner scanner, Task tasks) {
+
+        switch (userInput) {
+            case "add" -> {
+
+                // Add task
+                System.out.println("Task: ");
+                userInput = scanner.nextLine();
+                tasks.addNewTask(userInput);
+
+                // Wait for next command
+                System.out.println("Next prompt: ");
+                userInput = scanner.nextLine();
+                handlePrompt(userInput, scanner, tasks);
+            }
+            case "show" -> {
+
+                // Show task
+                tasks.showTasks();
+
+                // Wait fot next command
+                System.out.println("Next prompt: ");
+                userInput = scanner.nextLine();
+                handlePrompt(userInput, scanner, tasks);
+            }
+            case "delete" -> {
+
+                // Show task for easier UX
+                tasks.showTasks();
+
+                // Delete task
+                System.out.println("Type the task you want to delete: ");
+                userInput = scanner.nextLine();
+                tasks.deleteTask(userInput);
+
+                // Wait for next command
+                System.out.println("Next prompt: ");
+                userInput = scanner.nextLine();
+                handlePrompt(userInput, scanner, tasks);
+            }
+            case "help" -> {
+                System.out.println("""
+                        Instructions:\s
+                         add: adds a new task\
+                        \s
+                         show: shows all tasks\
+                        \s
+                         delete: deletes a task\
+                        \s
+                         exit: to end the program""");
+                handlePrompt(userInput, scanner, tasks);
+            }
+
+            case "exit" ->
+
+                // End program
+                    System.out.println("Program ending!");
+            default -> {
+                System.out.println("Invalid input");
+                System.out.println("""
+                        Instructions:\s
+                         add: adds a new task\
+                        \s
+                         show: shows all tasks\
+                        \s
+                         delete: deletes a task\
+                        \s
+                         exit: to end the program""");
+                handlePrompt(userInput, scanner, tasks);
+            }
         }
     }
 }
--- src/main/java/com/nathanasowata/Task.java
+++ src/main/java/com/nathanasowata/Task.java
@@ -1,4 +1,59 @@
 package com.nathanasowata;
 
+import java.awt.desktop.SystemEventListener;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
 public class Task {
+    private Map<String, String> tasks =  new HashMap<>();
+    private final Set<String> taskSet = this.tasks.keySet();
+
+    public Map<String, String> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(Map<String, String> tasks) {
+        this.tasks = tasks;
+    }
+
+    public ArrayList<String> allCompletedTasks(){
+        ArrayList<String> completedTasks = new ArrayList<>();
+
+
+        for(String task : this.taskSet){
+            if(this.tasks.get(task).equals("completed")){
+                completedTasks.add(task);
+            }
+        }
+        return completedTasks;
+    }
+
+    public ArrayList<String> allPendingTasks(){
+        ArrayList<String> pendingTasks = new ArrayList<>();
+
+        for (String task : this.taskSet){
+            if(this.tasks.get(task).equals("pending")){
+                pendingTasks.add(task);
+            }
+        }
+        return pendingTasks;
+    }
+
+    public void showTasks(){
+        System.out.println("Status \t \t Tasks");
+
+        for(String task : this.taskSet){
+            System.out.println(this.tasks.get(task) + "\t \t" + task);
+        }
+    }
+
+    public void addNewTask(String task){
+        this.tasks.put(task, "pending");
+    }
+
+    public void deleteTask(String task){
+        this.tasks.remove(task);
+    }
 }
